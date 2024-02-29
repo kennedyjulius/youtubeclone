9d5bd08fb5012516b62e5588c2927d55e3f10f71
Kennedy Mutugi Julius <kennedymutugi111@gmail.com>
2024-03-01 01:24:08 +0300

    added stripes of code

--- lib/widgets/youtube_player_custom.dart
+++ lib/widgets/youtube_player_custom.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-
 class YoutubePlayerCustomPlayer extends StatefulWidget {
   final String videoId = '0sacQ4oo-P0';
   const YoutubePlayerCustomPlayer({super.key});
@@ -25,16 +24,61 @@ class _YoutubePlayerCustomPlayerState extends State<YoutubePlayerCustomPlayer> {
 
   void _onPlayerStateChange() {
     if (_controller.value.playerState == PlayerState.playing) {
-      print("The Video is playing");
+      final currentTime = _controller.value.position.inSeconds;
+      final currentSubtitle = subtitle.firstWhere((subtitle) =>
+          currentTime >= subtitle.start && currentTime <= subtitle.end);
+
+      //update the UI with the current subtitle
+
+      setState(() {
+        subtitleText = currentSubtitle.text;
+      });
     }
+  }
+
+  //List for custom Subtitle and diplaying duration
+
+  List<Subtitle> subtitle = [
+    Subtitle(start: 2, end: 10, text: "Animated Container Widget In Flutter"),
+    Subtitle(start: 10, end: 200, text: "You can add your custom subtitle"),
+    Subtitle(start: 2, end: 10, text: "Animated Container Widget In Flutter"),
+  ];
+
+  String subtitleText = "";
+
+  //disposing
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [YoutubePlayer(controller: _controller)],
-      ),
-    );
+        body: Stack(
+      children: [
+        YoutubePlayer(controller: _controller),
+        Padding(
+          padding: const EdgeInsets.only(left: 8, top: 190),
+          child: Text(
+            subtitleText,
+            style: TextStyle(fontSize: 17, color: Colors.white),
+          ),
+        )
+      ],
+    ));
   }
+}
+
+class Subtitle {
+  final int start;
+  final int end;
+  final String text;
+
+  Subtitle({
+    required this.start,
+    required this.end,
+    required this.text,
+  });
 }
